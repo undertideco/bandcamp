6d4a1b1ee80ad4c6c8a584bddb377c7ee9cf6335
Duncan Leo <hello@duncanleo.me>
2019-09-07 14:55:01 +0800

    use pointer for ReleaseDate

--- model.go
+++ model.go
@@ -4,11 +4,11 @@ import "time"
 
 // Media represents media
 type Media struct {
-	Type        string    `json:"type"`
-	ArtworkURL  string    `json:"artwork_url"`
-	Title       string    `json:"title"`
-	Album       string    `json:"album"`
-	Artist      string    `json:"artist"`
-	ReleaseDate time.Time `json:"release_date"`
-	URL         string    `json:"url"`
+	Type        string     `json:"type"`
+	ArtworkURL  string     `json:"artwork_url"`
+	Title       string     `json:"title"`
+	Album       string     `json:"album"`
+	Artist      string     `json:"artist"`
+	ReleaseDate *time.Time `json:"release_date"`
+	URL         string     `json:"url"`
 }
